f91d91ad86f6ed187300479955eac56ee9fdbd8f
귀띵 <135992700+gwidding@users.noreply.github.com>
2023-10-06 11:12:27 +0900

    받는 건 ok, 보낼 때도 받아져서 2번 뜸

---
+++ websocket/myscript.js
@@ -0,0 +1,89 @@
+let num = 0;
+
+url = "ws://localhost:55000";
+w = new WebSocket(url);
+
+w.onopen = function() {
+    console.log("open");
+    // w.send("thank you for accepting web socket");
+}
+
+w.onmessage = function(e) {
+    console.log("메시지왔어요 " + e.data);
+    sendText(e.data);
+}
+w.onclose = function(e) {
+    log("closed");
+}
+
+
+
+$(function() {  
+    textBox = $("#inputText");
+    // 클릭이나 엔터 이벤트
+    $("#enterButton").on('click', addText(textBox.val()));
+    $("#inputText").on("keypress", function(key) {
+        if (key.which == 13) {
+            addText(textBox.val());
+        }
+    });
+
+    // 버튼 색상 활성화
+    $("#inputText").on("input", function() {
+        $("#enterButton").attr("class","turnOn"); 
+    });
+});
+
+function sendText(msg) {
+    if (!msg == "") {
+        $("#enterButton").attr("class", "button"); // 보냈으니 버튼 꺼짐
+
+        var currNum = num;
+        $("#content").append("<div class='text' id='text_" + currNum + "'>" + msg 
+                            +"<img src='x.png' class='delete' onclick='delText(" + currNum +")'>" + "</div>");
+        w.send($("#inputText").val());           
+
+        $("#del_" + currNum).on('click', function() {
+            const delIdx = $(this).data("index");
+            delText(delIdx);
+        });
+
+        $("#inputText").val('');
+        num++; 
+    } else {
+        console.log("입력 값 없음");
+    }
+
+    $("#content").stop().animate({ scrollTop: "+=1000px" }, "fast");
+
+}
+
+
+
+function addText(msg) {
+    if (!msg == "") {
+        $("#enterButton").attr("class", "button"); // 보냈으니 버튼 꺼짐
+
+        var currNum = num;
+        $("#content").append("<div class='text' id='text_" + currNum + "'>" + msg 
+                            +"<img src='x.png' class='delete' onclick='delText(" + currNum +")'>" + "</div>");
+        w.send($("#inputText").val());           
+
+        $("#del_" + currNum).on('click', function() {
+            const delIdx = $(this).data("index");
+            delText(delIdx);
+        });
+
+        $("#inputText").val('');
+        num++; 
+    } else {
+        console.log("입력 값 없음");
+    }
+
+    $("#content").stop().animate({ scrollTop: "+=1000px" }, "fast");
+}
+
+
+function delText(idx) {
+    $("#text_" + idx).remove();
+}
