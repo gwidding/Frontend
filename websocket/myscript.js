b576675d27b1986018daf176a6a4085a36a92198
귀띵 <135992700+gwidding@users.noreply.github.com>
2023-10-10 15:34:41 +0900

    채팅 기록도 불러오기

--- websocket/myscript.js
+++ websocket/myscript.js
@@ -6,23 +6,28 @@ socket = new WebSocket(url);
 
 socket.onopen = function() {
     console.log("open");
-    // w.send("thank you for accepting web socket");
 }
 socket.onmessage = function(e) {
-    var msgData = e.data.split(":");
-    console.log("보낸사람 ID :  ", msgData[0]);
-    if (msgData[0] !== myID) {
-        console.log("메세지 왔어요~", msgData[1]);
-        receiveText(e.data);
-    } else {
-        console.log("전송 완료");
-    }
+    var msg = e.data.split(":");
+    contextData(msg[0], msg[1]);
+    fetch("http://localhost:8080/websocket/dataadd.jsp?userid=" + myID + 
+       "&message=" + msg);
 }
 socket.onclose = function(e) {
     console.log("closed");
 }
+function inputChatData(data) {
+    console.log(data);
+    for (var index=0; index < data.length; index++) {
+        contextData(data.data[index].userid, data.data[index].message);
+    }
+}
 
-$(function() {  
+$(function() {
+    fetch("http://localhost:8080/websocket/dataread.jsp")
+    .then(response => response.json())
+    .then(json => inputChatData(json))
+
     $("#idButton").on("click", function() {
         myID = $("#name").val();
     });
@@ -30,11 +35,15 @@ $(function() {
     // 클릭이나 엔터 이벤트
     textBox = $("#inputText");
     $("#enterButton").on('click', function() {
-        sendText(textBox.val())
+        contextData(myID, textBox.val());
+        var myMsg = myID + ": " + $("#inputText").val();
+        socket.send(myMsg);
     });
     $("#inputText").on("keypress", function(key) {
         if (key.which == 13) {
-            sendText(textBox.val());
+            contextData(myID, textBox.val());
+            var myMsg = myID + ": " + $("#inputText").val();
+            socket.send(myMsg);
         }
     });
 
@@ -45,45 +54,44 @@ $(function() {
 });
 
 
-function sendText(msg) {
-    if (!msg == "") {
-        fetch("http://localhost:8080/websocket/dataadd.jsp?userid=" + myID +
-                "&message=" + msg);
-
-        $("#enterButton").attr("class", "button"); // 보냈으니 버튼 꺼짐
-
-        var myMsg = myID + ": " + $("#inputText").val();
-        var currNum = num;
-        $("#content").append("<div class='text' id='text_" + currNum + "'>"
-                            +"<span>" + msg + "</span>" 
-                            +"<img src='x.png' class='delete' onclick='delText(" + currNum +")'>" 
-                            +"</div>");
-    
-        socket.send(myMsg);
-        
-        $("#inputText").val('');
-        num++; 
+function contextData(currentID, msg) {
+    if (currentID == myID) {
+        textIdNum = "text_";
+        textBoxClassName = "textBox";
     } else {
-        // console.log("입력 값 없음");
+        textIdNum = "text2_"
+        textBoxClassName = "textBox2"
     }
-}
-
-function receiveText(msg) {
     if (!msg == "") {
         $("#enterButton").attr("class", "button"); // 보냈으니 버튼 꺼짐
-
         var currNum = num;
-        $("#content").append("<div class='text2' id='text2_" + currNum + "'>"
+        $("#content").append("<div class='" + textBoxClassName + "'"
+                                       + "id='" + textIdNum + currNum + "'>"
                             +"<span>" + msg + "</span>" 
                             +"<img src='x.png' class='delete' onclick='delText(" + currNum +")'>" 
                             +"</div>");
         $("#inputText").val('');
         num++; 
-    } else {
-        // console.log("입력 값 없음");
     }
 }
 
 function delText(idx) {
     $("#text_" + idx).remove();
 }
+
+// function receiveText(msg) {
+//     if (!msg == "") {
+//         $("#enterButton").attr("class", "button"); // 보냈으니 버튼 꺼짐
+
+//         var currNum = num;
+//         $("#content").append("<div class='text2' id='text2_" + currNum + "'>"
+//                             +"<span>" + msg + "</span>" 
+//                             +"<img src='x.png' class='delete' onclick='delText(" + currNum +")'>" 
+//                             +"</div>");
+//         $("#inputText").val('');
+//         num++; 
+//     } else {
+//         // console.log("입력 값 없음");
+//     }
+// }
+
