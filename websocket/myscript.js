fa69066989ef3c2aa34b14d085bad0468d608bd3
귀띵 <ok88eun77@naver.com>
2023-10-11 00:55:31 +0900

    메서드 간략화, id 데이터도 가져옴
    확인 필요

--- websocket/myscript.js
+++ websocket/myscript.js
@@ -6,25 +6,23 @@ socket = new WebSocket(url);
 
 socket.onopen = function() {
     console.log("open");
-    // w.send("thank you for accepting web socket");
 }
 socket.onmessage = function(e) {
     var msgData = e.data.split(":");
-    console.log("보낸사람 ID :  ", msgData[0]);
-    if (msgData[0] !== myID) {
-        console.log("메세지 왔어요~", msgData[1]);
-        receiveText(e.data);
-    } else {
-        console.log("전송 완료");
-    }
+    console.log("보낸사람 ID: ", senderID);
+    console.log("메세지 왔어요~", message);
+    sendMessage(senderID, message);
 }
 socket.onclose = function(e) {
     console.log("closed");
 }
 function inputChatData(data) {
     console.log(data);
-    for (var index=0; index< data.length; index++) {
-        console.log(data.data[index].userid, data.data[index].message);
+    for (var index=0; index < data.length; index++) {
+        var senderID = data.data[index].userid; // 저장된 메시지의 보낸 사람 ID
+        var message = data.data[index].message;  // 저장된 메시지 내용
+        console.log(senderID, message);
+        sendMessage(senderID, message);
     }
 }
 
@@ -54,45 +52,66 @@ $(function() {
     });
 });
 
+function sendText(senderID, message) {
+    if (!message == "") {
+        fetch("http://localhost:8080/websocket/dataadd.jsp?userid=" + senderID +
+            "&message=" + message);
 
-function sendText(msg) {
-    if (!msg == "") {
-        fetch("http://localhost:8080/websocket/dataadd.jsp?userid=" + myID +
-                "&message=" + msg);
+        $("#enterButton").attr("class", "button"); // 버튼 꺼짐
 
-        $("#enterButton").attr("class", "button"); // 보냈으니 버튼 꺼짐
-
-        var myMsg = myID + ": " + $("#inputText").val();
+        var myMsg = senderID + ": " + message;
         var currNum = num;
-        $("#content").append("<div class='text' id='text_" + currNum + "'>"
-                            +"<span>" + msg + "</span>" 
-                            +"<img src='x.png' class='delete' onclick='delText(" + currNum +")'>" 
-                            +"</div>");
+        var messageClass = senderID === myID ? "text1" : "text2"; // 보낸 메시지와 받은 메시지를 구분
+        $("#content").append("<div class='" + messageClass + "' id='text_" + currNum + "'>"
+            + "<span>" + myMsg + "</span>"
+            + "<img src='x.png' class='delete' onclick='delText(" + currNum + ")'>"
+            + "</div>");
+
+        $("#inputText").val('');
+        num++;
+    } else {
+        // console.log("입력 값 없음");
+    }
+}
+
+// function sendText(msg) {
+//     if (!msg == "") {
+//         fetch("http://localhost:8080/websocket/dataadd.jsp?userid=" + myID +
+//                 "&message=" + msg);
+
+//         $("#enterButton").attr("class", "button"); // 보냈으니 버튼 꺼짐
+
+//         var myMsg = myID + ": " + $("#inputText").val();
+//         var currNum = num;
+//         $("#content").append("<div class='text' id='text_" + currNum + "'>"
+//                             +"<span>" + msg + "</span>" 
+//                             +"<img src='x.png' class='delete' onclick='delText(" + currNum +")'>" 
+//                             +"</div>");
     
-        socket.send(myMsg);
+//         socket.send(myMsg);
         
-        $("#inputText").val('');
-        num++; 
-    } else {
-        // console.log("입력 값 없음");
-    }
-}
+//         $("#inputText").val('');
+//         num++; 
+//     } else {
+//         // console.log("입력 값 없음");
+//     }
+// }
 
-function receiveText(msg) {
-    if (!msg == "") {
-        $("#enterButton").attr("class", "button"); // 보냈으니 버튼 꺼짐
+// function receiveText(msg) {
+//     if (!msg == "") {
+//         $("#enterButton").attr("class", "button"); // 보냈으니 버튼 꺼짐
 
-        var currNum = num;
-        $("#content").append("<div class='text2' id='text2_" + currNum + "'>"
-                            +"<span>" + msg + "</span>" 
-                            +"<img src='x.png' class='delete' onclick='delText(" + currNum +")'>" 
-                            +"</div>");
-        $("#inputText").val('');
-        num++; 
-    } else {
-        // console.log("입력 값 없음");
-    }
-}
+//         var currNum = num;
+//         $("#content").append("<div class='text2' id='text2_" + currNum + "'>"
+//                             +"<span>" + msg + "</span>" 
+//                             +"<img src='x.png' class='delete' onclick='delText(" + currNum +")'>" 
+//                             +"</div>");
+//         $("#inputText").val('');
+//         num++; 
+//     } else {
+//         // console.log("입력 값 없음");
+//     }
+// }
 
 function delText(idx) {
     $("#text_" + idx).remove();
